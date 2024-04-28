6152948961d9a227533fe835cc7a36ddf51e85cb
MASUMHD <mrmasum2k4@gmail.com>
2024-04-28 16:56:33 +0600

    only one item code push

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -36,6 +36,13 @@ async function run() {
         res.send(result);
     })
 
+    app.get('/addItems/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id:  new ObjectId(id) };
+        const result = await addItemCollection.findOne(query);
+        res.send(result);
+    })
+
     app.post('/addItem', async (req, res) => {
         const newItem = req.body;
         console.log(newItem);
@@ -51,6 +58,7 @@ async function run() {
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
+
   }
 }
 run().catch(console.dir);
